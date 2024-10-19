68de4d51a5b75e1f6a2777178bab6474b836c7a6
Sergei Droganov <sergei@droganov.com>
2024-10-19 20:37:07 +0200

    fixed linter warning

--- src/utils/getOpenApiSpec.ts
+++ src/utils/getOpenApiSpec.ts
@@ -1,13 +1,13 @@
+import type { AnyOpenApi } from '../openApi';
+
 import RefParser from '@apidevtools/json-schema-ref-parser';
 
-import { AnyOpenApi } from '../openApi';
-
 /**
- * Load and parse te open api spec. If the file extension is ".yml" or ".yaml"
+ * Load and parse the open api spec. If the file extension is ".yml" or ".yaml"
  * we will try to parse the file as a YAML spec, otherwise we will fall back
  * on parsing the file as JSON.
  * @param location: Path or url
  */
 export const getOpenApiSpec = async (location: string): Promise<AnyOpenApi> => {
-    return (await RefParser.bundle(location, location, {})) as AnyOpenApi;
+    return await RefParser.bundle(location, location, {});
 };
